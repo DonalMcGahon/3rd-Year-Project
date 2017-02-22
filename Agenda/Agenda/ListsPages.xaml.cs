aa8a1cc46463243ba2c7aaeb23bcf09f4ef170b6
DonalMcGahon <dmcgahon1234@hotmail.com>
2017-02-22 20:43:49 +0000

    Added Suggestions for Grocery List & added some comments
    When user enters text for the grocery list they will be given some
    suggestions as to what to add to their grocery list. Added some comments
    into code.

--- Agenda/Agenda/ListsPages.xaml.cs
+++ Agenda/Agenda/ListsPages.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -73,6 +74,7 @@ namespace Agenda
 
         }
 
+        // Delete TO-DO List
         private void button5_Click(object sender, RoutedEventArgs e)
         {
             conn.DropTable<List>();
@@ -98,11 +100,38 @@ namespace Agenda
         }
 
         // Grocery List Buttons and Textboxs etc.
-        private void AddGrocery_TextChanged(object sender, TextChangedEventArgs e)
+
+        private async void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
         {
+            // If user enters text go into this if statement
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+            {
+                string text = sender.Text;
+                if (sender.Text.Length > 0)
+                {
+                    sender.ItemsSource = await Task<string[]>.Run(() => { return this.GetSuggestions(text); });
+                }
+                else
+                {
+                    sender.ItemsSource = new string[] { "No suggestions..." };
+                }
+            }
+        }
+
+        // Grocery Suggestions
+        private string[] suggestions = new string[] { "asparagus", "apples", "avacado", "alfalfa", "acorn squash", "almond", "arugala", "artichoke", "applesauce", "asian noodles", "antelope", "ahi tuna", "albacore tuna", "Apple juice", "Avocado roll", "Bruscetta", "bacon", "black beans", "bagels", "baked beans", "BBQ", "bison", "barley", "beer", "bisque", "bluefish", "bread", "broccoli", "buritto", "babaganoosh", "Cabbage", "cake", "carrots", "carne asada", "celery", "cheese", "chicken", "catfish", "chips", "chocolate", "chowder", "clams", "coffee", "cookies", "corn", "cupcakes", "crab", "curry", "cereal", "chimichanga", "dates", "dips", "duck", "dumplings", "donuts", "eggs", "enchilada", "eggrolls", "English muffins", "edimame", "eel sushi", "fajita", "falafel", "fish  (might be too vague)", "franks", "fondu", "French toast", "French dip", "Garlic", "ginger", "gnocchi", "goose", "granola", "grapes", "green beans", "Guancamole", "gumbo", "grits", "Graham crackers", "ham", "halibut", "hamburger", "honey", "huenos rancheros", "hash browns", "hot dogs", "haiku roll", "hummus", "ice cream", "Irish stew", "Indian food", "Italian bread", "jambalaya", "jelly / jam", "jerky", "jalapeño", "kale", "kabobs", "ketchup", "kiwi", "kidney beans", "kingfish", "lobster", "Lamb", "Linguine", "Lasagna", "Meatballs", "Moose", "Milk", "Milkshake", "Noodles", "Ostrich", "Pizza", "Pepperoni", "Porter", "Pancakes", "Quesadilla", "Quiche", "Reuben", "Spinach", "Spaghetti", "Tater tots", "Toast", "Venison", "Waffles", "Wine", "Walnuts", "Yogurt", "Ziti", "Zucchini" };
+
+        private string[] GetSuggestions(string text)
+        {
+            string[] result = null;
+
+            result = suggestions.Where(x => x.Contains(text)).ToArray();
+
+            return result;
 
         }
 
+        // Done button adding to Grocery List
         private void done_Click(object sender, RoutedEventArgs e)
         {
             var g = conn.Insert(new gList()
@@ -112,6 +141,12 @@ namespace Agenda
             });
             // Updates the ItemsSource for ListView
             groceryList.ItemsSource = conn.Table<gList>();
+        }
+
+        // Delete Grocery List
+        private void groceryDelete_Click(object sender, RoutedEventArgs e)
+        {
+            conn.DropTable<gList>();
         }
     }
     }
